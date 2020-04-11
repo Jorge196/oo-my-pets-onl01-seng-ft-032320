8a5267a5f833770bf60f66b53bbbbb1f0e681edc
Jorge Ramirez <joramirez96@gmail.com>
2020-04-11 23:12:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
 
   @@dogs = []
 
-  def initialize(name, owner, mood="nervous")
+  def initialize(name, owner, mood)
     @name = name
     @owner = owner
     @mood = mood
@@ -15,7 +15,7 @@ class Dog
 
   def name
     @name
-  end 
+  end
 
   def self.all
     @@dogs
