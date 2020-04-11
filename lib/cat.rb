87da80eab3f14bf4a8912359a8d82c3477065af4
Jorge Ramirez <joramirez96@gmail.com>
2020-04-11 23:02:24 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,25 @@
 class Cat
   # code goes here
+  attr_accessor  :owner, :mood
+  attr_reader  :name
+
+  @@cats = []
+
+  def initialize(name, owner, mood="nervous")
+    @name = name
+    @owner = owner
+    owner.cats << self
+    @mood = mood
+    @@cats << self
+  end
+
+  def self.all
+    @@cats
+  end
+
+
+
+
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,76 @@
 class Owner
   # code goes here
+  @@all= []
+  attr_accessor 
+  attr_reader :species, :name, :cats, :dogs, :mood
+
+  # Instance Methods #
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @dogs = []
+    @cats = []
+    @species = "human"
+  end
+
+  def cats
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+
+  def dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
+
+  def say_species
+     "I am a #{@species}."
+  end
+
+  def buy_cat(name)
+     Cat.new(name, self)
+  end
+
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+
+  def walk_dogs
+    @dogs.each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def feed_cats
+  end
+
+
+
+
+  def sell_pets
+
+  end
+
+  def list_pets
+
+  end
+
+  # Class Methods #
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.size
+  end
 end
